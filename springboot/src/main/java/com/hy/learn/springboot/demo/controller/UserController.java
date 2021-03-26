0e000d0710e29558cd52f43b504d278a0e840fa3
honins <honins@qq.com>
2021-03-26 09:12:05 +0800

    update: 更新依赖

--- springboot/src/main/java/com/hy/learn/springboot/demo/controller/UserController.java
+++ springboot/src/main/java/com/hy/learn/springboot/demo/controller/UserController.java
@@ -2,9 +2,7 @@ package com.hy.learn.springboot.demo.controller;
 
 import com.hy.learn.springboot.demo.common.CommonResult;
 import com.hy.learn.springboot.demo.pojo.vo.UserVO;
-import com.hy.learn.springboot.learn.conditional.RandDataComponent;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
-
-    @Autowired
-    RandDataComponent randDataComponent;
 
     @ApiOperation(value = "取用户信息")
     @GetMapping(value = "/info")
