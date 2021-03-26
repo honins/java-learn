8ea77cb25f827a1434d2496ffd1ceaa3ea6a86fd
honins <honins@qq.com>
2021-03-26 10:37:00 +0800

    add: 添加jdk动态代理中 动态代理类的class文件

---
+++ base/src/main/java/com/hy/learn/base/design/proxy/JdkProxySourceClass.java
@@ -0,0 +1,44 @@
+package com.hy.learn.base.design.proxy;
+
+import com.hy.learn.base.design.proxy.impl.Hello;
+import sun.misc.ProxyGenerator;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * @author Created by hy
+ * @date on 2021/3/26 10:26
+ */
+public class JdkProxySourceClass {
+
+    public static void writeClassToDisk(String path) {
+        byte[] classFile = ProxyGenerator.generateProxyClass("$proxy1", new Class[]{Hello.class});
+
+        FileOutputStream fos = null;
+
+        try {
+            fos = new FileOutputStream(path);
+            fos.write(classFile);
+            fos.flush();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if (fos != null){
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        writeClassToDisk("D:\\java\\idea_workspace\\$Proxy1.class");
+    }
+
+}
