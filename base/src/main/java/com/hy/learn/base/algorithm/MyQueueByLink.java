d44a62320ec4a8c05b0d16339d26f1b661a6221a
honins <honins@qq.com>
2021-04-11 17:32:19 +0800

    add: 添加栈 的内容，并手写用数组实现的栈:MyQueueByList和用链表实现的栈：MyQueueByLink

---
+++ base/src/main/java/com/hy/learn/base/algorithm/MyQueueByLink.java
@@ -0,0 +1,95 @@
+package com.hy.learn.base.algorithm;
+
+/**
+ * @author home-pc
+ * @program: java-learn
+ * @description: 手写队列，使用数组实现
+ * @date 2021-04-11 13:38:27
+ */
+public class MyQueueByLink {
+
+    class Node {
+        private Object data;
+        private Node next;
+    }
+
+    private int count;
+
+    private int capacity;
+
+    private Node head;
+
+    private Node tail;
+
+    private Node queue;
+
+    public MyQueueByLink(int capacity) {
+        Node node = new Node();
+        head = node;
+        tail = node;
+        queue = node;
+        for (int i = 0; i < capacity - 1; i++){
+            node.next = new Node();
+        }
+        this.capacity = capacity;
+        this.count = 0;
+    }
+
+    public boolean enqueue(Object item) {
+        if (tail.next == null){
+            if (count == capacity){
+                return false;
+            }
+
+            queue = head;
+            for (int i = 0; i< capacity - count; i++){
+                tail.next = new Node();
+            }
+        }
+        tail.data = item;
+        count++;
+        tail = tail.next;
+        return true;
+    }
+
+    public Object dequeue() {
+        if (count == 0){
+            return null;
+        }
+        Object result = head.data;
+        head = head.next;
+        count--;
+        return result;
+    }
+
+    private void printAll(){
+        Node temp = queue;
+        for (int i = 0; i < count; i++) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        MyQueueByLink myQueue = new MyQueueByLink(3);
+        myQueue.enqueue("111");
+        myQueue.enqueue("222");
+        myQueue.enqueue("333");
+        myQueue.enqueue("444");
+        myQueue.enqueue("555");
+
+        myQueue.printAll();
+
+        myQueue.dequeue();
+        myQueue.dequeue();
+
+        myQueue.printAll();
+
+        myQueue.enqueue("666");
+        myQueue.enqueue("777");
+        myQueue.enqueue("888");
+
+        myQueue.printAll();
+    }
+
+}
---
+++ base/src/main/java/com/hy/learn/base/algorithm/MyQueueByList.java
@@ -0,0 +1,75 @@
+package com.hy.learn.base.algorithm;
+
+/**
+ * @author home-pc
+ * @program: java-learn
+ * @description: 手写队列，使用数组实现
+ * @date 2021-04-11 13:38:27
+ */
+public class MyQueueByList {
+
+    private Object[] items;
+
+    private int capacity;
+
+    private int head = 0;
+
+    private int tail = 0;
+
+    public MyQueueByList(int capacity) {
+        this.items = new Object[capacity];
+        this.capacity = capacity;
+    }
+
+    public boolean enqueue(Object item) {
+        if (tail == capacity) {
+            //队列已占满
+            if (head == 0) {
+                return false;
+            }
+//            //移动队列
+//            int newTail = 0;
+//            int temp = head;
+//            for (int i = 0; temp < tail; i++) {
+//                items[i] = items[temp];
+//                temp++;
+//                newTail = i + 1;
+//            }
+//            tail = newTail;
+//            head = 0;
+//            items[tail] = item;
+//            tail++;
+            //比注释部分实现更加优雅
+            for (int i = head; i < tail; i++) {
+                items[i - head] = items[i];
+            }
+            tail -= head;
+            head = 0;
+        }
+
+        items[tail] = item;
+        tail++;
+        return true;
+    }
+
+    public Object dequeue() {
+        if (head == tail) {
+            return null;
+        }
+        return items[head++];
+    }
+
+    public static void main(String[] args) {
+        MyQueueByList myQueueByList = new MyQueueByList(3);
+        myQueueByList.enqueue("111");
+        myQueueByList.enqueue("222");
+        myQueueByList.enqueue("333");
+        myQueueByList.enqueue("444");
+        myQueueByList.enqueue("555");
+        myQueueByList.dequeue();
+        myQueueByList.dequeue();
+        myQueueByList.enqueue("666");
+        myQueueByList.enqueue("777");
+    }
+
+}
