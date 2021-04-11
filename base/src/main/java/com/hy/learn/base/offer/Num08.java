39d870785a6b7c769b93d27a7fe4fdef1a3ff43f
honins <honins@qq.com>
2021-04-11 22:43:00 +0800

    add: 剑指offer 第八题题解。第二种符合题意的解法

--- base/src/main/java/com/hy/learn/base/offer/Num08.java
+++ base/src/main/java/com/hy/learn/base/offer/Num08.java
@@ -1,6 +1,6 @@
 package com.hy.learn.base.offer;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 /**
  * @author home-pc
@@ -12,53 +12,42 @@ public class Num08 {
 
     static class CQueue {
 
-        private ArrayList<Integer> stack;
+        private LinkedList<Integer> stack;
 
-        private int stackBottom;
-
-        private int stackTop;
-
-        private ArrayList<Integer> reverseStack;
+        private LinkedList<Integer> reverseStack;
 
         public CQueue() {
-            stack = new ArrayList<Integer>();
-            stackBottom = 0;
-            stackTop = 0;
-
-            reverseStack = new ArrayList<Integer>();
+            stack = new LinkedList<>();
+            reverseStack = new LinkedList<>();
         }
 
         public void appendTail(int value) {
-            stack.add(value);
-            stackTop++;
-
-            if (stackTop == 1){
-                reverseStack = stack;
-            }
-            for (int i = stackTop - 1; i< stackBottom; i--){
-                reverseStack.add(stack.get(i));
-            }
+            stack.push(value);
         }
 
         public int deleteHead() {
-            if (stackTop == stackBottom){
-                return -1;
+            if (reverseStack.isEmpty()) {
+                if (stack.isEmpty()) {
+                    return -1;
+                }
+
+                while (!stack.isEmpty()) {
+                    reverseStack.push(stack.pop());
+                }
+                return reverseStack.pop();
             }
-            Integer head = reverseStack.get(stackBottom);
-            if (head == null){
-                return -1;
-            }
-            stackBottom ++;
-            return head;
+            return reverseStack.pop();
         }
     }
 
     public static void main(String[] args) {
         CQueue cQueue = new CQueue();
+        System.out.println(cQueue.deleteHead());
         cQueue.appendTail(1);
         cQueue.appendTail(2);
+        cQueue.appendTail(3);
         System.out.println(cQueue.deleteHead());
-        System.out.println(cQueue.deleteHead());
+        cQueue.appendTail(4);
         System.out.println(cQueue.deleteHead());
     }
 
--- doc/剑指offer/剑指 Offer 08 用两个栈实现队列.md
+++ doc/剑指offer/剑指 Offer 08 用两个栈实现队列.md
@@ -117,3 +117,61 @@ class Solution {
 我也不期望双100
 
 不过学习了栈和队列的基础知识后，这题做起来挺顺利的。有思路了十几分钟代码就写完了。然后花了10分钟调试边界值。
+
+# 符合题意的做法
+
+## 思路
+上一个做法完成后，再看了一遍题目，感觉上面做法并不符合题意，上面做法实例上没有使用栈，就是单纯的数组操作。
+
+看了一下别人的解答发现
+
+>使用java的同学请注意，如果你使用Stack的方式来做这道题，会造成速度较慢； 原因的话是Stack继承了Vector接口，而Vector底层是一个Object[]数组，那么就要考虑空间扩容和移位的问题了。 
+>可以使用LinkedList来做Stack的容器，因为LinkedList实现了Deque接口，所以Stack能做的事LinkedList都能做，其本身结构是个双向链表，扩容消耗少。 但是我的意思不是像100%代码那样直接使用一个LinkedList当做队列，那确实是快，但是不符题意。 贴上代码，这样的优化之后，效率提高了40%，超过97%。
+
+看来可以使用LinkedList来做Stack的容器
+也是做了一个符合题意的解法，还花了些时间
+
+## 解法
+```java
+
+ static class CQueue {
+
+        private LinkedList<Integer> stack;
+
+        private LinkedList<Integer> reverseStack;
+
+        public CQueue() {
+            stack = new LinkedList<>();
+            reverseStack = new LinkedList<>();
+        }
+
+        public void appendTail(int value) {
+            stack.push(value);
+        }
+
+        public int deleteHead() {
+            if (reverseStack.isEmpty()) {
+                if (stack.isEmpty()) {
+                    return -1;
+                }
+
+                while (!stack.isEmpty()) {
+                    reverseStack.push(stack.pop());
+                }
+                return reverseStack.pop();
+            }
+            return reverseStack.pop();
+        }
+    }
+
+```
+
+需要注意的是
+1. 我刚开始尝试在appendTail填充反转Stack。后来发现这个做法不对，不能解决 push1 push2 pop1 push3 pop2的情形，第二次pop2应该输出push2。若每次push就填充则返回的是push3
+2. deleteHead方法中，首先要全部输出完reverseStack，再往reverseStack中填充，否则也会出现1的错误情况
+
+结果
+>执行结果：通过
+ 显示详情
+ 执行用时：55 ms, 在所有 Java 提交中击败了87.93%的用户
+ 内存消耗：46.7 MB, 在所有 Java 提交中击败了49.56%的用户
