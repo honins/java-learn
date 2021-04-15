dedb675356a66f38fc09cd8b6481459fefbe22c6
honins <honins@qq.com>
2021-04-15 13:05:03 +0800

    add: 添加插入排序代码

--- base/src/main/java/com/hy/learn/base/algorithm/Sort.java
+++ base/src/main/java/com/hy/learn/base/algorithm/Sort.java
@@ -17,9 +17,9 @@ public class Sort {
     static int[] bubbleSort(int[] a, int n) {
 
         for (int i = 0; i < n; i++) {
-            for (int m = 0; m < n -1 - i; m++) {
+            for (int m = 0; m < n - 1 - i; m++) {
                 if (a[m] > a[m + 1]) {
-                    int temp = a[m+1];
+                    int temp = a[m + 1];
                     a[m + 1] = a[m];
                     a[m] = temp;
                 }
@@ -29,9 +29,36 @@ public class Sort {
         return a;
     }
 
+
+
+    static int[] insertionSort(int[] a, int n) {
+        for (int i = 1; i < n; ++i){
+
+            int inject = i - 1;
+            int value = a[i];
+
+            for(; inject >= 0; --inject){
+                if (a[inject] > value){
+                    a[inject + 1] = a[inject];
+                }else {
+                    break;
+                }
+            }
+
+            a[inject+1] = value;
+
+        }
+
+        return a;
+
+    }
+
+
+
     public static void main(String[] args) {
-        int[] a = {3,4,2,5,6,1};
-        int[] ints = bubbleSort(a, 6);
+        int[] a = {3, 4, 2, 5, 6, 1};
+//        int[] ints = bubbleSort(a, 6);
+        int[] ints = insertionSort(a, 6);
         for (int i = 0; i < ints.length; i++) {
             System.out.print(a[i]);
         }
